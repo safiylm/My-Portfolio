7de21e13eb2fe05e04427fee6a4598806ad005db
safiylm <121391404+safiylm@users.noreply.github.com>
2024-10-13 22:00:45 +0200

    add services

---
+++ shared/prestations.php
@@ -0,0 +1,80 @@
+<div class="mes-prestations">
+
+    <h2>Mes services </h2>
+    <div class="d-flex flex-row flex-wrap align-items-center justify-content-center">
+
+        <div class="item d-flex flex-column flex-wrap align-items-center justify-content-center">
+            <h3>Création de site web</h3>
+            <a>En savoir plus</a>
+        </div>
+        
+        <div class="item d-flex flex-column flex-wrap align-items-center justify-content-center">
+            <h3>Création app mobile</h3>
+            <a>En savoir plus</a>
+        </div>
+        
+        <div class="item d-flex flex-column flex-wrap align-items-center justify-content-center">
+            <h3>Conception de logo</h3>
+            <a>En savoir plus</a>
+        </div>
+        
+        <div class="item d-flex flex-column flex-wrap align-items-center justify-content-center">
+            <h3>Cartes de visite et print</h3>
+            <a>En savoir plus</a>
+        </div>
+        
+        
+        
+       <div class="item d-flex flex-column flex-wrap align-items-center justify-content-center">
+            <h3>Marketing digital</h3>
+            <a>En savoir plus</a>
+        </div>
+        
+           
+       <div class="item d-flex flex-column flex-wrap align-items-center justify-content-center">
+            <h3>Donne cours de mathématiques pour tout niveau</h3>
+            <a>En savoir plus</a>
+        </div>
+       
+    </div>
+</div>
+
+<style>
+    .mes-prestations{
+        background-color: #5336b6;
+        width:100vw;
+        height: auto;
+        padding: 20px;
+    }
+    
+     .mes-prestations h2{
+         color: white;
+     }
+    
+    .mes-prestations .item{
+        background-color: white;
+        margin:10px;
+        border-radius:10px;
+        padding:10px;
+        height : 120px;
+        
+        
+    }
+    
+        .mes-prestations .item h3,
+        .mes-prestations .item a{
+            margin: 10px;
+            text-align:center;
+            font-size:large;
+        }
+        
+        .mes-prestations .item a{
+             font-size:medium;
+             color: #5336b6;
+
+        }           
+
+        .mes-prestations .item a:hover{
+            text-decoration:underline;
+        }           
+</style>
