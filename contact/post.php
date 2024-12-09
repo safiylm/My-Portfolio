27e3a8e9856b35ee4c02e6278aa782de9d7e5f22
safiylm <121391404+safiylm@users.noreply.github.com>
2024-12-09 21:17:01 +0100

    improve "mail send" to client and prestataire

--- contact/post.php
+++ contact/post.php
@@ -1,53 +1,60 @@
 <!Doctype html>
-<?php 
+<?php
 
 include('../shared/sendmail.php');
 
 
-$nom=  $_POST['nom'];
+$nom =  $_POST['nom'];
 $email = $_POST["email"];
 $message = $_POST['message'];
 
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    // invalid emailaddress
-    echo "adresse invalide ";
+  // invalid emailaddress
+  echo "adresse invalide ";
 }
 
-if(
-    
-    preg_match("/^[a-zA-Z-' ]*$/",$nom) && !empty($nom) &&
-    filter_var($email, FILTER_VALIDATE_EMAIL) &&  !empty($email) &&
-    !empty($message)  
-)
-{
-  
-//
-  //  echo "<div style='display:none'>";
-    $title = "Nouveau message de ". $nom;
-    $message_for_ayhan =  $nom ." vous a écrit :  <br/><br/>". "'".$message."' <br/><br/> Son email: ".$email ;
-  
-    $back1 = sendmail("safinazyilmaz54@gmail.com", $title , $message_for_ayhan, $title );
-   
-    
-    $message_Reception ="Nous avons bien reçu votre message. Nous mettons tout en oeuvre pour vous répondre dans les plus brefs délais. Si toutefois, votre requête est laissée sans réponse, nous vous encourageons à nous contacter par téléphone au <a href='tel:0768259414'>07 68 25 94 14</a>.";
+if (
 
-    $back2 = sendmail("safinazyilmaz54@gmail.com" , "Message reçu" , $message_Reception , "Message reçu" );
-    
-   // echo "</div>";
-     
+  preg_match("/^[a-zA-Z-' ]*$/", $nom) && !empty($nom) &&
+  filter_var($email, FILTER_VALIDATE_EMAIL) &&  !empty($email) &&
+  !empty($message)
+) {
+
+  //
+  echo "<div style='display:none'>";
+  $title = "Nouveau message de " . $nom;
+  $message_for_prestataire =
+    "Bonjour,<br/><br/>" .
+    "Nous souhaitons vous informer que nous avons reçu un message de la part de " . $nom . " et voici son mail " . $email . " <br/><br/><strong>" .
+    $message . "</strong> <br/><br/><br/>" .
+    "Cordialement </br> ";
+
+  $back1 = sendmail("safinazyilmaz54@gmail.com", $title, $message_for_prestataire, $title);
+
+
+  $message_for_client =
+    "<div class='div-email'>" .
+    "Bonjour " . $nom . ",<br/><br/>" .
+    "Nous vous remercions pour votre message que nous avons bien reçu. Nous en prenons connaissance avec attention et reviendrons vers vous dans les meilleurs délais pour répondre à vos besoins.<br/><br/>" .
+    "N’hésitez pas à nous contacter si vous avez d’autres précisions ou demandes en attendant.<br/><br/>" .
+    "Cordialement,<br/>" .
+    "Safinaz YILMAZ<br/>" .
+    "Developpeuse Web Freelance" .
+    "</div>" .
+    "<style>.div-email{font-size:19px;}</style>";
+
+  $back2 = sendmail($email, "Bonne reception de votre message", $message_for_client, "Bonne reception de votre message");
+
+  echo "</div>";
 }
 
-echo $back1. " " . $back2;
+echo $back1 . " " . $back2;
 
-if($back1 == 'Successfully sent' && $back2 == 'Successfully sent'){
-        echo "<script> document.location.href='index.php?sent=ok';</script>";
-}else{
-     echo "<script> document.location.href='index.php?sent=error';</script>";
+if ($back1 == 'Successfully sent' && $back2 == 'Successfully sent') {
+  echo "<script> document.location.href='index.php?sent=ok';</script>";
+} else {
+  echo "<script> document.location.href='index.php?sent=error';</script>";
 }
 
 ?>
-
-<script>
-    
-</script>
