103f261eaa192799007c1bf8858b32fb21c4585c
safiylm <121391404+safiylm@users.noreply.github.com>
2024-12-09 19:40:40 +0100

    describe prestation web

---
+++ shared/prestation-web.php
@@ -0,0 +1,58 @@
+<section>
+    
+    <h3>Création de site web</h3>
+     <div class="d-flex align-ietms-center justify-content-center">
+         <div class="ayhansarl-projet">
+         <a href="https://ayhansarl.fr" target="_blank">
+             <img id="img_ayhansarl" src="../assets/ayhansarl.jpg" alt="AYHAN SARL"/>
+              <img id="img_ayhansarl" src="../assets/ayhansarl2.jpg" alt="AYHAN SARL"/>
+            <p>Ma Création AYHAN SARL</p> 
+         </a>
+         </div>
+        <div class="prestation-texte">
+            <div>Un site web idéale pour les entreprises, indépendants ou associations souhaitant présenter leur activité, renforcer leur visibilité en ligne et attirer des clients potentiels.
+            </div> 
+            <br/>
+            <ul>
+                <li>Professionnalisme : Un site conçu pour refléter l’image de votre marque.</li>
+                <li>Visibilité : Optimisé pour le référencement naturel (SEO).</li>
+                <li>Simplicité : Gestion facile de vos contenus grâce à une interface utilisateur intuitive.</li>
+                <li>Accompagnement : Un partenaire à vos côtés pour un projet réussi.</li>
+                <li>Délai de Réalisation : Entre 2 à 4 semaines selon la complexité du projet.</li>
+                <li>Prix : dépend de vos besoins spécifiques (nombre de pages, fonctionnalités, design personnalisé). Une estimation sur mesure sera fournie après l’analyse du projet.</li>
+            </ul>
+        </div>
+    </div>
+  
+</section>
+
+<style>
+    #img_ayhansarl{
+        width : 30vw;
+        height:auto;
+        margin: 10px auto;
+        display:block;
+    }
+    
+    .ayhansarl-projet{
+           width : 40vw;
+           
+           a{
+               color:black;
+               font-weight: 200;
+               font-size: 20px;
+              
+             
+           } p{ display: block; text-align: center; padding: 15px;}
+    }
+
+  
+    
+    .prestation-texte{
+        width:60vw;
+        font-weight: lighter;
+        font-size: 20px;
+        line-height:40px;
+    }
+
+</style>
