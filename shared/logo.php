3a72f1419bbb98236abd5bb4cf3d091afcbc2ac7
safiylm <121391404+safiylm@users.noreply.github.com>
2024-12-09 19:39:58 +0100

    improve logo prestation

--- shared/logo.php
+++ shared/logo.php
@@ -1,49 +1,80 @@
 <section class="conception-logo">
-    <h2>Conception de logo</h2>
-
-
-    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="1800">
-                <img src="./assets/logo/1.png" class="d-block " alt="...">
+<h3>Création de logos <br/>Donnez une identité unique à votre marque</h3>
+    <div class="d-flex align-items-center justify-content-center">
+        <div id="carouselExampleInterval" class="carousel carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="1800">
+                    <img src="./assets/logo/1.png" class="d-block " alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="1800">
+                    <img src="./assets/logo/2.png" class="d-block" alt="...">
+                </div>
+    
+                <div class="carousel-item" data-bs-interval="1800">
+                    <img src="./assets/logo/3.png" class="d-block" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="1800">
+                    <img src="./assets/logo/4.png" class="d-block" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="1800">
+                    <img src="./assets/logo/5.png" class="d-block" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="1800">
+                    <img src="./assets/logo/6.png" class="d-block" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="1800">
+                    <img src="./assets/logo/7.png" class="d-block" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="1800">
+                    <img src="./assets/logo/8.png" class="d-block" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="1800">
+                    <img src="./assets/logo/9.png" class="d-block" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="1800">
+                    <img src="./assets/logo/10.png" class="d-block" alt="...">
+                </div>
+    
             </div>
-            <div class="carousel-item" data-bs-interval="1800">
-                <img src="./assets/logo/2.png" class="d-block" alt="...">
-            </div>
-
-            <div class="carousel-item" data-bs-interval="1800">
-                <img src="./assets/logo/3.png" class="d-block" alt="...">
-            </div>
-            <div class="carousel-item" data-bs-interval="1800">
-                <img src="./assets/logo/4.png" class="d-block" alt="...">
-            </div>
-            <div class="carousel-item" data-bs-interval="1800">
-                <img src="./assets/logo/5.png" class="d-block" alt="...">
-            </div>
-            <div class="carousel-item" data-bs-interval="1800">
-                <img src="./assets/logo/6.png" class="d-block" alt="...">
-            </div>
-            <div class="carousel-item" data-bs-interval="1800">
-                <img src="./assets/logo/7.png" class="d-block" alt="...">
-            </div>
-            <div class="carousel-item" data-bs-interval="1800">
-                <img src="./assets/logo/8.png" class="d-block" alt="...">
-            </div>
-            <div class="carousel-item" data-bs-interval="1800">
-                <img src="./assets/logo/9.png" class="d-block" alt="...">
-            </div>
-            <div class="carousel-item" data-bs-interval="1800">
-                <img src="./assets/logo/10.png" class="d-block" alt="...">
-            </div>
-
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+        
+        <div class="prestation-texte">
+            <div>
+            <br/>
+            Un logo est bien plus qu’une simple image : c’est le reflet de votre identité, de vos valeurs et de votre vision. Notre service de conception de logo vous accompagne dans la création d’un visuel unique et mémorable, parfaitement adapté à votre activité et à votre public cible. Ensemble, nous définissons vos besoins et transformons vos idées en un logo qui saura captiver et inspirer confiance.
+            <br/><br/>
+            A partir de 25€.  
+        </div>
     </div>
 </section>
+
+
+<style>
+  
+    
+    .prestation-texte{
+        width:50vw;
+        font-weight: lighter;
+        font-size: 20px;
+        line-height:40px;
+    }
+    
+    #carouselExampleInterval{
+        
+        width: 25vw !important;
+        height: 25vw;
+        
+        img{
+            width: 25vw !important;
+        height: 25vw; 
+        }
+  }
+</style>
